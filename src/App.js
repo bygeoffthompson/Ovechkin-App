fa95b2cea9d91385a1880eab2d346676659c6e79
Geoff Thompson <gthompson@unleashed-technologies.com>
2026-02-12 16:47:29 -0500

    .

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import ReactGA from 'react-ga4';
 
-ReactGA.initialize('G-5RVBYX6N0S');
+//ReactGA.initialize('G-5RVBYX6N0S');
 
 const totalGoals = 919;
 
