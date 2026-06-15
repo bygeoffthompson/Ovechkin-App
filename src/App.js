0d37e6487c9a4d04f251c33897ddbafdb8377fda
Geoff Thompson <gthompson@unleashed-technologies.com>
2026-06-15 18:59:40 -0400

    .

--- src/App.js
+++ src/App.js
@@ -313,10 +313,7 @@ function SearchForm({jsonData}) {
                         <div className="align-items-start buttons-group d-flex flex-row gap-3 mb-4">
                             <div className="d-flex flex-column gap-3 league-buttons">
                                 <button onClick={(event) => shuffle()} title="Shuffle" type="button">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" className="bi bi-shuffle" viewBox="0 0 16 16">
-                                        <path fill-rule="evenodd" d="M0 3.5A.5.5 0 0 1 .5 3H1c2.202 0 3.827 1.24 4.874 2.418.49.552.865 1.102 1.126 1.532.26-.43.636-.98 1.126-1.532C9.173 4.24 10.798 3 13 3v1c-1.798 0-3.173 1.01-4.126 2.082A9.6 9.6 0 0 0 7.556 8a9.6 9.6 0 0 0 1.317 1.918C9.828 10.99 11.204 12 13 12v1c-2.202 0-3.827-1.24-4.874-2.418A10.6 10.6 0 0 1 7 9.05c-.26.43-.636.98-1.126 1.532C4.827 11.76 3.202 13 1 13H.5a.5.5 0 0 1 0-1H1c1.798 0 3.173-1.01 4.126-2.082A9.6 9.6 0 0 0 6.444 8a9.6 9.6 0 0 0-1.317-1.918C4.172 5.01 2.796 4 1 4H.5a.5.5 0 0 1-.5-.5"/>
-                                        <path d="M13 5.466V1.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384l-2.36 1.966a.25.25 0 0 1-.41-.192m0 9v-3.932a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384l-2.36 1.966a.25.25 0 0 1-.41-.192"/>
-                                    </svg>Shuffle
+                                   <img alt="Shuffle icon" src="/icons/shuffle.svg"/>Shuffle
                                 </button>
                                 <button onClick={(event) => filterGoal(['NHL Regular'])} title="NHL Regular Season" type="button">
                                     <img alt="NHL logo" src="/teams/NHL.svg"/>NHL
@@ -332,13 +329,13 @@ function SearchForm({jsonData}) {
                                     <img alt="KHL logo" src="/teams/KHL.svg" />KHL
                                 </button>
                                 <button onClick={(event) => filterGoal(['Olympics'])} title="Olympic" type="button">
-                                    <img alt="Olympics logo" src="/icons/Olympics.svg" />Olympics
+                                    <img alt="Olympics logo" src="/icons/olympics.svg" />Olympics
                                 </button>
                                 <button onClick={(event) => filterGoal(['World Championships'])} title="World Championship" type="button">
-                                    <img alt="Trophy logo" src="/icons/Trophy.svg" />Worlds
+                                    <img alt="Trophy logo" src="/icons/trophy.svg" />Worlds
                                 </button>
                                 <button onClick={worldCup} title="World Cup" type="button">
-                                    <img alt="Cup logo" src="/icons/Cup.svg" />World&nbsp;Cup
+                                    <img alt="Cup logo" src="/icons/cup.svg" />World&nbsp;Cup
                                 </button>
                             </div>
                             <div className="d-flex flex-column gap-3">
