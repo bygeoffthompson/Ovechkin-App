be7dc806b4a827a8ed0a8789e58272c92aa9b00a
Geoff Thompson <gthompson@unleashed-technologies.com>
2026-08-15 17:21:16 -0400

    OnThisDay

--- src/useOnThisDay.js
+++ src/useOnThisDay.js
@@ -1,13 +1,13 @@
 import { useMemo } from 'react'
 
-export function useTodaysGoals(jsonData) {
+export function useOnThisDay(jsonData) {
     const now = new Date()
     const month = now.getMonth() + 1
     const day = now.getDate()
 
-    const todaysGoals = useMemo(() =>
+    const onThisDayGoals = useMemo(() =>
         jsonData.filter(item => item.month === month && item.day === day),
     [jsonData, month, day])
 
-    return { todaysGoals, month, day }
+    return { onThisDayGoals, month, day }
 }
