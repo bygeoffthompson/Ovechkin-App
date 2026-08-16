61f47a1fbdcc81604b13d2191bf4447c97c8a12a
Geoff Thompson <gthompson@unleashed-technologies.com>
2026-08-15 21:54:10 -0400

    GA

--- src/App.js
+++ src/App.js
@@ -104,12 +104,11 @@ function SearchForm({jsonData}) {
     useUrlQuery(setSearchGoal, setSearchText, searchSubmit)
 
     useEffect(() => {
-        searchResults.slice(0, 50).forEach(result => {
-            _ga?.event({
-                category: 'Results',
-                action: 'Goal Results',
-                label: result.goal.toString()
-            })
+        if (searchResults.length === 0) return
+        _ga?.event({
+            category: 'Results',
+            action: 'Goal Views',
+            label: searchResults[0].goal.toString()
         })
     }, [searchResults])
 
@@ -495,7 +494,7 @@ function SearchForm({jsonData}) {
                             const [goalInt, goalDec] = result.goal.toString().split('.')
                             return (
                             <Accordion.Item key={result.goal} data-jersey={result.jersey} data-league={LEAGUE[result.league]} eventKey={index.toString()}>
-                                <div className="accordion-header"><Accordion.Button onClick={lazyLoadFrame}>
+                                <div className="accordion-header"><Accordion.Button onClick={(e) => { lazyLoadFrame(); if (e.currentTarget.getAttribute('aria-expanded') === 'false') { _ga?.event({ category: 'Results', action: 'Goal Views', label: result.goal.toString() })} }}>
                                     <div className="align-items-center d-flex gap-1 justify-content-start w-100">
                                         <strong className="align-items-center d-flex goal-count">
                                             {result.league !== 1 && <small className="fw-bold me-1">{result.league === 2 ? 'Playoffs' : result.league === 5 ? 'Worlds' : LEAGUE[result.league]}</small>}
