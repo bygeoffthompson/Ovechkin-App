2dff6be99d38c75235dcfb7f6502270a88604049
Geoff Thompson <gthompson@unleashed-technologies.com>
2026-08-18 16:28:36 -0400

    Time Button

--- src/App.js
+++ src/App.js
@@ -506,7 +506,7 @@ function SearchForm({jsonData}) {
                             </select>}
                         </div>
                     )}
-                    {isPending && <div className="alert alert-light d-inline-block" role="alert"><span className="h6">Loading Ovechkin Goals</span></div>}
+                    {isPending && <div className="alert alert-light d-inline-block opacity-50" role="alert"><span className="h6">Loading Ovechkin Goals</span></div>}
                     {tooMany > 0 && <div className="alert alert-light d-inline-block" role="alert"><span className="h6">Please Refine Your Search</span></div>}
                     <Accordion className="goal-accordion shadow-lg w-100" defaultActiveKey="0" flush>
                         {!isPending && sortedResults.map((result, index) => {
@@ -606,12 +606,14 @@ function WelcomeMessage({jsonData, onGoalSelect}) {
                     <div className="align-items-center d-flex flex-row flex-wrap gap-3 mb-3">
                         <p className="h6 m-0">Time</p>
                         <button className="button refresh" onClick={refreshTime} title="Refresh" type="button">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" className="bi bi-stopwatch" viewBox="0 0 16 16">
-                                <path d="M8.5 5.6a.5.5 0 1 0-1 0v2.9h-3a.5.5 0 0 0 0 1H8a.5.5 0 0 0 .5-.5z"/>
-                                <path d="M6.5 1A.5.5 0 0 1 7 .5h2a.5.5 0 0 1 0 1v.57c1.36.196 2.594.78 3.584 1.64l.012-.013.354-.354-.354-.353a.5.5 0 0 1 .707-.708l1.414 1.415a.5.5 0 1 1-.707.707l-.353-.354-.354.354-.013.012A7 7 0 1 1 7 2.071V1.5a.5.5 0 0 1-.5-.5M8 3a6 6 0 1 0 .001 12A6 6 0 0 0 8 3"/>
-                            </svg>
+                            <div className="align-items-center d-flex flex-row gap-1 justify-content-center">
+                                {time}
+                                <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" className="bi bi-stopwatch" viewBox="0 0 16 16">
+                                    <path d="M8.5 5.6a.5.5 0 1 0-1 0v2.9h-3a.5.5 0 0 0 0 1H8a.5.5 0 0 0 .5-.5z"/>
+                                    <path d="M6.5 1A.5.5 0 0 1 7 .5h2a.5.5 0 0 1 0 1v.57c1.36.196 2.594.78 3.584 1.64l.012-.013.354-.354-.354-.353a.5.5 0 0 1 .707-.708l1.414 1.415a.5.5 0 1 1-.707.707l-.353-.354-.354.354-.013.012A7 7 0 1 1 7 2.071V1.5a.5.5 0 0 1-.5-.5M8 3a6 6 0 1 0 .001 12A6 6 0 0 0 8 3"/>
+                                </svg>
+                            </div>
                         </button>
-                        <span className="badge p-2">{time}</span>
                         {atThisTimeGoals.length > 0 ? atThisTimeGoals.map(goal => (
                             <button className="button" key={goal.goal} onClick={() => onGoalSelect(goal.goal)} title="At This Time" type="button">
                                 <small className="d-block">{goal.time} {PERIOD_NAME[goal.period]}</small>
