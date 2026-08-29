f2eb42f5986af6bcddb8b4cdddecd442b602f6d3
Geoff Thompson <gthompson@unleashed-technologies.com>
2026-08-29 14:36:32 -0400

    .

--- src/LeagueFilters.js
+++ src/LeagueFilters.js
@@ -5,8 +5,6 @@ const LEAGUES = LEAGUE_ORDER.map(key => ({ key, ...LEAGUE_META[key] }))
 export default function LeagueFilters({ leagueCounts, disabledLeagues, isAnimating, anim, totalDisplay, activeLeagueGoals, randomGoal, jsonData, searchGoal, handleGoalNumber }) {
     return (
         <div className="d-flex flex-wrap align-items-stretch gap-1 mb-3">
-            <label htmlFor="goal-number" hidden>Goal</label>
-            <input className="text-center" id="goal-number" min={0} max={leagueCounts[1]} placeholder="#" step="any" type="number" value={searchGoal} onChange={handleGoalNumber}/>
             {LEAGUES.map(({ key, label, title }) => (
                 <button key={key} className="button counter" disabled={isAnimating || !!disabledLeagues[key]} onClick={() => randomGoal(jsonData.filter(item => item.league === key))} title={title} type="button">
                     <div className="h4 m-0" data-goals={leagueCounts[key]}>{anim(leagueCounts[key])}</div>
@@ -16,6 +14,8 @@ export default function LeagueFilters({ leagueCounts, disabledLeagues, isAnimati
             <button className="button" disabled={isAnimating || activeLeagueGoals.length === 0} onClick={() => randomGoal(activeLeagueGoals)} title="Total" type="button">
                 <span className="h2 m-0" data-goals={activeLeagueGoals.length}>{isAnimating ? anim(activeLeagueGoals.length) : totalDisplay}</span>
             </button>
+            <label htmlFor="goal-number" hidden>Goal</label>
+            <input className="fw-bold text-center" id="goal-number" min={0} max={leagueCounts[1]} placeholder="#" step="any" type="number" value={searchGoal} onChange={handleGoalNumber}/>
         </div>
     )
 }
